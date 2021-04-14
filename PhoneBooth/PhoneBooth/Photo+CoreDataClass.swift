739d09d4ffd6fc1ff1cc2f619e2d6e0547bfbfb6
Jerin App <jerinapp@icloud.com>
2021-04-14 05:21:57 +0100

    created Photo model for CoreData

---
+++ PhoneBooth/PhoneBooth/Photo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Photo+CoreDataClass.swift
+//  
+//
+//  Created by Jamal on 14/04/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
